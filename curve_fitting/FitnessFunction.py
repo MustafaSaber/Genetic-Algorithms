0c656beab01ec0780f67967b29e416968f27ff1f
Khaled Hamed <khaled.hamedt@gmail.com>
2018-10-31 13:20:35 +0200

    refactor

--- curve_fitting/FitnessFunction.py
+++ curve_fitting/FitnessFunction.py
@@ -3,20 +3,21 @@
 
 
 def calculate_y(chromosome, points):
-    """ chromosome list has the current coefficient values
+    """ Chromosome list has the current coefficient values
      the length of the list is it's degree"""
     y_calculated = []
-    for i in range(len(points)):
-        y_temp = chromosome[0]
-        for j in range(1, len(chromosome)):
-            y_temp += chromosome[j] * (points[i].x ** j)
+    for point in points:
+        y_temp = 0
+        for j in range(len(chromosome)):
+            y_temp += chromosome[j] * (point.x ** j)
         y_calculated.append(y_temp)
     return y_calculated
 
 
 def fitness(chromosome, points):
+    """ Mean square error """
     y_calculated, total_sum = calculate_y(chromosome, points), 0
     for i in range(len(points)):
         total_sum += ((points[i].y - y_calculated[i]) ** 2)
-    return (1/len(points)) * total_sum
+    return (1 / len(points)) * total_sum
 
