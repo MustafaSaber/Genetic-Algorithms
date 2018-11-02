35b90279cb2545af6b2c2f711718be9239acc8a2
Khaled Hamed <khaled.hamedt@gmail.com>
2018-11-02 11:44:44 +0200

    rebase

--- curve_fitting/GeneticAlgo.py
+++ curve_fitting/GeneticAlgo.py
@@ -1,31 +1,27 @@
-from Object import Object
+from curve_fitting.Object import Object
 import random
 import bisect
-import FitnessFunction as ff
+import curve_fitting.FitnessFunction as ff
 import matplotlib.pyplot as plt
 from tqdm import trange
 from joblib import Parallel, delayed
 import multiprocessing
-import math
-from graph import Graph
-import time
-
 
 pop_size = 500
 # Maximum number of generations
 max_generations = 10000
 
 # Probability of crossover between [ 0.4 , 0.7 ]
-p_crossover = 0.7
+p_crossover = 0.65
 
 # Probability of mutation between [ 0.001 , 0.1 ]
 p_mutation = 0.1
 
-graph = Graph()
+dependency_factor = 0.5
 
 
 def create_population(degree):
-    return [[random.uniform(-10, 10) for _ in range(degree + 1)] for _ in range(pop_size)]
+    return [[random.uniform(-10, 10) for _ in range(degree+1)] for _ in range(pop_size)]
 
 
 def mutate(chromosome, generation_number):
@@ -34,12 +30,12 @@ def mutate(chromosome, generation_number):
         val = (chromosome[i] + 10) if r1 <= 0.5 else (10 - chromosome[i])
         power = (1 - generation_number/max_generations) ** dependency_factor
         delta = val * (1 - r2 ** power)
-        if r3 <= p_mutation * (1 - generation_number / max_generations):
-            chromosome[i] = random.uniform(-10, 10)
+        if r3 <= p_mutation:
+            chromosome[i] = chromosome[i] - delta if r1 <= 0.5 else chromosome[i] + delta
 
 
 def cross_over(parent1, parent2):
-    r1 = random.randint(1, len(parent1) - 1)
+    r1 = random.randint(1, len(parent1)-1)
     r2 = random.uniform(0, 1)
     parent1, parent2 = list(parent1), list(parent2)
     if r2 <= p_crossover:
@@ -48,16 +44,6 @@ def cross_over(parent1, parent2):
         return child1, child2
     else:
         return parent1, parent2
-
-
-def cross_over_non_uniform(parent1, parent2):
-    child = [list(parent1), list(parent2)]
-    for i in range(len(parent1)):
-        r2 = random.uniform(0, 1)
-        if r2 <= p_crossover:
-            # Swap
-            child[0][i], child[1][i] = child[1][i], child[0][i]
-    return child[0], child[1]
 
 
 # Will be used in selection
@@ -87,9 +73,7 @@ def genetic_algorithm(pop, points, generation_number):
         r1, r2 = random.uniform(0, summation), random.uniform(0, summation)
         idx1 = bisect.bisect_left(fitness_array_cumlative, r1)
         idx2 = bisect.bisect_left(fitness_array_cumlative, r2)
-
-        Offspring1, Offspring2 = cross_over_non_uniform(pop[idx1], pop[idx2])
-
+        (Offspring1, Offspring2) = cross_over(pop[idx1], pop[idx2])
         mutate(Offspring1, generation_number)
         mutate(Offspring2, generation_number)
         next_pop.append(Offspring1)
@@ -103,7 +87,6 @@ def genetic_algorithm(pop, points, generation_number):
         m1, m2 = fitness_array.index(max(fitness_array)), next_gen_fitness.index(max(next_gen_fitness))
         next_gen.append(pop[m1])
         next_gen.append(next_pop[m2])
-
         pop.remove(pop[m1])
         next_pop.remove(next_pop[m2])
         fitness_array.remove(max(fitness_array))
@@ -117,15 +100,6 @@ def genetic_algorithm(pop, points, generation_number):
     gen_fitness = pop_fitness(next_gen, points)
     best_val_old_pop = max(gen_fitness)
     best_chromosome = next_gen[gen_fitness.index(max(gen_fitness))]
-
-    # x_axis = [i.x for i in points]
-    # for i, chromosome in enumerate(next_gen):
-    #     if i == 10:
-    #         break
-    #     time.sleep(1)
-    #     print(chromosome)
-    #     y_calculated = ff.calculate_y(chromosome, points)
-    #     graph.update_example(x_axis, y_calculated)
 
     return next_gen, best_val_old_pop, best_chromosome
 
@@ -143,20 +117,47 @@ def run_testcase(n, d, x, y):
     return max_value, max_chromosome
 
 
-class Point():
-    def __init__(self, x, y):
-        self.x = float(x)
-        self.y = float(y)
+def main():
+    # n = []
+    # d = []
+    # x = []
+    # y = []
+    # # Read all input from file
+    # with open('input.txt', 'r') as infile:
+    #     t = int(infile.readline())
+    #     for i in range(t):
+    #         (n1, n2) = infile.readline().split()
+    #         n.append(int(n1))
+    #         d.append(int(n2))
+    #         x.append([])
+    #         y.append([])
+    #         for j in range(n[i]):
+    #             a, b = infile.readline().split()
+    #             x[i].append(a)
+    #             y[i].append(b)
+    #
+    # num_cores = multiprocessing.cpu_count()
+    # results = Parallel(n_jobs=num_cores)(delayed(run_testcase)(n[i], d[i], x[i], y[i])
+    #                      for i in trange(t, desc='Total', ascii=True))
+    #
+    # with open("output.txt", 'w') as outfile:
+    #     for case, val, chromosome in enumerate(results):
+    #         outfile.write('Case: %d \n' % (i+1))
+    #         for f in chromosome:
+    #             outfile.write(str(f) + ' ')
+    #         outfile.write(" value: %d \n" % val)
 
-
-class TestCase():
-    def __init__(self, n, degree, idx):
-        self.n = n
-        self.degree = degree
-        self.idx = idx
-        self.points = []
-        self.graph = Graph()
-
+    infile = open('input.txt', 'r')
+    outfile = open('output.txt', 'w')
+    # outfile2 = open('output2.txt', 'w')
+    test_cases = int(infile.readline())
+    for i in range(test_cases):
+        (n, d) = infile.readline().split()
+        n_points, degree = int(n), int(d)
+        points = []
+        for j in range(n_points):
+            (x, y) = infile.readline().split()
+            points.append(Object(float(x), float(y)))
         population = create_population(degree)
         max_val, max_chromosome, count = 0.0, [], 0
         for y in range(max_generations):
@@ -180,11 +181,10 @@ class TestCase():
         y_calculated = ff.calculate_y(max_chromosome, points)
         # for z in range(len(points)):
         #    outfile2.write('%f %f\n' % (points[z].x , y_calculated[z]))
-
-        # plt.plot(x_axis, y_calculated, linestyle='-.')
-        # plt.show()
-
+        plt.plot(x_axis, y_calculated, linestyle='-.')
+        plt.show()
     outfile.close()
+    infile.close()
 
 
 if __name__ == '__main__':
